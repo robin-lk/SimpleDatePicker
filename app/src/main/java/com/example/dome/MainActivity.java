f1641bab58b5d6b7cd741b77287fc76a05c69733
Lubin <wang_junjie007@163.com>
2021-12-09 09:19:06 +0800

    noo

--- app/src/main/java/com/example/dome/MainActivity.java
+++ app/src/main/java/com/example/dome/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.lk.simple.DatePickerFragmentDialog;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatePickerFragmentDialog dialog=new DatePickerFragmentDialog();
+        dialog.show(getSupportFragmentManager(),"ss");
     }
 }
